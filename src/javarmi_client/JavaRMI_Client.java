eeff5877db266c76bf03c93a7ec4d4784f4347d2
santensuru <djuned.ong@gmail.com>
2015-03-16 23:04:43 +0700

    bisa :D (local)

--- src/javarmi_client/JavaRMI_Client.java
+++ src/javarmi_client/JavaRMI_Client.java
@@ -4,11 +4,16 @@
  * and open the template in the editor.
  */
 package javarmi_client;
+import imagermi.ImageInterface;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.awt.image.MemoryImageSource;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
@@ -24,18 +29,52 @@ public class JavaRMI_Client {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
-        try {
-            // TODO code application logic here
-            File file = new File("1.jpg");
-            BufferedImage image = ImageIO.read(file);
-            ImageInterface stub = (ImageInterface)Naming.lookup("rmi://10.151.12.201:5000/image");
-            BufferedImage dest = stub.toBW(image);
-            File fbw = new File("BW.jpg");
-            if (!ImageIO.write(dest, "JPEG", fbw)) {
-                throw new RuntimeException("Unexpected error writing image");
+        // TODO code application logic here
+        int i = 2, j;
+        for (j=1; j<=i; j++) {
+        
+            try {
+
+                System.out.println("Read file ...");
+                File file = new File(String.valueOf(j)+".jpg");
+                BufferedImage image = ImageIO.read(file);
+
+                int x, y;
+                int h = image.getHeight();
+                int w = image.getWidth();
+                int[] input = new int[h*w];
+
+                for (y=0; y<h; y++) {
+                    for (x=0; x<w; x++) {
+                        input[x + w*y] = image.getRGB(x, y);
+                    }
+                }
+
+                System.out.println("File readed.");
+
+                System.out.println("Try connect ...");
+                Registry registry = LocateRegistry.getRegistry();
+                ImageInterface stub = (ImageInterface) registry.lookup("toBW");
+                System.out.println("Connected.");
+
+                System.out.println("Do RMI ...");
+                int[] output = stub.toBW(0, 0, w, h, input, 0, w);
+                BufferedImage dest = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
+
+                for (y=0; y<h; y++) {
+                    for (x=0; x<w; x++) {
+                        dest.setRGB(x, y, output[x + w*y] << 16 | output[x + w*y] << 8 | output[x + w*y]);
+                    }
+                }
+
+                File fbw = new File("BW-"+String.valueOf(j)+".jpg");
+                if (!ImageIO.write(dest, "JPEG", fbw)) {
+                    throw new RuntimeException("Unexpected error writing image");
+                }
+
+            } catch (NotBoundException | MalformedURLException | RemoteException ex) {
+                Logger.getLogger(JavaRMI_Client.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } catch (NotBoundException | MalformedURLException | RemoteException ex) {
-            Logger.getLogger(JavaRMI_Client.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
