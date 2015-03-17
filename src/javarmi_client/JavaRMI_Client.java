e492ea99ff688fa10f1fc903408bc36d50a5e557
santensuru <djuned.ong@gmail.com>
2015-03-17 11:34:36 +0700

    bisa :D (local ITS)
    +timer

--- src/javarmi_client/JavaRMI_Client.java
+++ src/javarmi_client/JavaRMI_Client.java
@@ -12,6 +12,7 @@ import java.net.MalformedURLException;
 import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
@@ -29,10 +30,14 @@ public class JavaRMI_Client {
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         int i = 10, j;
+        long time = 0;
         for (j=1; j<=i; j++) {
         
             try {
-
+                
+                Date date = new Date();
+                System.out.println(date);
+                
                 System.out.println("Read file ...");
                 File file = new File(String.valueOf(j)+".jpg");
                 BufferedImage image = ImageIO.read(file);
@@ -51,17 +56,18 @@ public class JavaRMI_Client {
                 System.out.println("File readed.");
 
                 System.out.println("Try connect ...");
-                Registry registry = LocateRegistry.getRegistry();
+                Registry registry = LocateRegistry.getRegistry("10.151.12.201");
                 ImageInterface stub = (ImageInterface) registry.lookup("toBW");
                 System.out.println("Connected.");
 
                 System.out.println("Do RMI ...");
+                
                 int[] output = stub.toBW(0, 0, w, h, input, 0, w);
                 BufferedImage dest = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
 
                 for (y=0; y<h; y++) {
                     for (x=0; x<w; x++) {
-                        dest.setRGB(x, y, output[x + w*y] << 16 | output[x + w*y] << 8 | output[x + w*y]);
+                        dest.setRGB(x, y, output[x + w*y]);
                     }
                 }
 
@@ -69,6 +75,9 @@ public class JavaRMI_Client {
                 if (!ImageIO.write(dest, "JPEG", fbw)) {
                     throw new RuntimeException("Unexpected error writing image");
                 }
+                time = new Date().getTime() - date.getTime();
+                System.out.println(date);
+                System.out.println("Done with time execution: "+etaConvert(time)+".");
 
             } catch (NotBoundException | MalformedURLException | RemoteException ex) {
                 Logger.getLogger(JavaRMI_Client.class.getName()).log(Level.SEVERE, null, ex);
@@ -76,5 +85,22 @@ public class JavaRMI_Client {
         }
         
     }
-    
+    private static String etaConvert(long l_d) {
+        String eta = "";
+        l_d /= 1000;
+        if (l_d >= 86400) {
+            eta += String.valueOf(l_d/86400) + " d ";
+            l_d %= 86400;
+        }
+        if (l_d >= 3600) {
+            eta += String.valueOf(l_d/3600) + " h ";
+            l_d %= 3600;
+        }
+        if (l_d >= 60) {
+            eta += String.valueOf(l_d/60) + " m ";
+            l_d %= 60;
+        }
+        eta += String.valueOf(l_d) + " s";
+        return eta;
+    }
 }
